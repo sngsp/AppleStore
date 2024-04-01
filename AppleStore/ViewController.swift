c238bb3793a5c3543b69ede0c86aa1a1a5e08b83
kimtaedam <incoverlet@gmail.com>
2024-04-01 19:15:25 +0900

    버튼 오토레이아웃 설정했습니다.

--- AppleStore/ViewController.swift
+++ AppleStore/ViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var iPhoneButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func iPhoneButtonPressed(_ sender: Any) {
+        
+        //iPhoneButton.image = UIImage(named: "가져오신 이미지 이름")
+    }
+    
 }
 
