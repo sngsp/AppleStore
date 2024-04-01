473e82a12b8e309d4b859bceebeda61614cfd904
sngsp <ssleerok@gmail.com>
2024-04-01 13:22:26 +0900

    The First Commit

---
+++ AppleStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleStore
+//
+//  Created by Seungseop Lee on 4/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
